a2983a51eb92862b9c4237ffe9928ecbc9ed88a8
Ross Bartlett <rossjbartlett@gmail.com>
2020-03-11 23:02:48 -0600

    cleanup header file

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import { Alert, Text, Dimensions } from 'react-native'
-// import LinearGradient from 'react-native-linear-gradient'
+
 import {
   Menu,
   MenuOptions,
@@ -13,80 +13,69 @@ import Icon from 'react-native-vector-icons/MaterialIcons'
 const ICON_SIZE = 24
 
 const WIDTH = Dimensions.get('window').width
-//   < LinearGradient
-// style = { styles.gradient }
-// locations = { [0, 1.0]}
-// colors = { ['#5ED2A0', '#339CB1']}
-//   />
+
+const menuOptionStyles = {
+  optionsContainer: {
+    // backgroundColor: 'green',
+    // width: 'min-content',
+  },
+  optionWrapper: {
+    // backgroundColor: 'yellow',
+    // width: 0,
+  },
+}
+const CustomMenu = () => {
+  return (
+    <Menu>
+      <MenuTrigger>
+        <Icon
+          name='more-vert'
+          size={ICON_SIZE}
+          color={'white'}
+        />
+      </MenuTrigger>
+      <MenuOptions>
+        <MenuOption>
+          <Text style={{ color: 'black' }}>Info</Text>
+          {/* TODO info menu? */}
+        </MenuOption>
+        <MenuOption
+          customStyles={menuOptionStyles}
+          onSelect={() => Alert.alert(
+            'Reset Game',
+            'Are you sure you want to reset?',
+            [
+              { text: 'Ask me later', onPress: () => console.log('Ask me later pressed') },
+              {
+                text: 'Cancel',
+                onPress: () => console.log('Cancel Pressed'),
+                style: 'cancel',
+              },
+              { text: 'OK', onPress: () => console.log('OK Pressed') },
+            ],
+            { cancelable: false },
+          )} >
+          <Text style={{ color: 'red' }}>Reset Game</Text>
+        </MenuOption>
+      </MenuOptions>
+    </Menu>
+  )
+}
+
 const Header = (title) => {
   return {
     title,
     headerLeft: null,
     headerStyle: {
-      backgroundColor: '#0E71FF',
-      // backgroundColor: 'linear-gradient(90deg, rgba(14,113,255,1) 50%, rgba(14,190,0,1) 100%)'
+      backgroundColor: '#0E71FF', // TODO blue constant?
     },
-    // headerBackground: (
-    // <LinearGradient
-    //   colors={['#a13388', '#10356c']}
-    //   style={{ flex: 1 }}
-    //   start={{ x: 0, y: 0 }}
-    //   end={{ x: 1, y: 0 }}
-    // />
-    // <Image
-    //   // resizeMode='contain'
-    //   style={{ width: 100, height: 200, resizeMode: 'contain'}}
-    //   source={require('../assets/gradient.png')}
-    // />
-    // ),
     headerTintColor: '#ffffff',
     headerTitleStyle: {
       fontWeight: 'bold',
       width: WIDTH - 75,
     },
-    headerRight: () => (
-      <Menu>
-        <MenuTrigger>
-          <Icon
-            name='more-vert'
-            size={ICON_SIZE}
-            color={'white'}
-          />
-        </MenuTrigger>
-        <MenuOptions>
-          <MenuOption
-            customStyles={optionsStyles}
-            onSelect={() => Alert.alert(
-              'Reset Game',
-              'Are you sure you want to reset?',
-              [
-                { text: 'Ask me later', onPress: () => console.log('Ask me later pressed') },
-                {
-                  text: 'Cancel',
-                  onPress: () => console.log('Cancel Pressed'),
-                  style: 'cancel',
-                },
-                { text: 'OK', onPress: () => console.log('OK Pressed') },
-              ],
-              { cancelable: false },
-            )} >
-            <Text style={{ color: 'red' }}>Reset Game</Text>
-          </MenuOption>
-        </MenuOptions>
-      </Menu>
-    ),
+    headerRight: () => CustomMenu(),
   }
-}
-
-const optionsStyles = {
-  optionsContainer: {
-    backgroundColor: 'green',
-    width: 'min-content',
-  },
-  optionWrapper: {
-    backgroundColor: 'yellow',
-    // width: 0,
-  },
 }
 
 export default (Header)
